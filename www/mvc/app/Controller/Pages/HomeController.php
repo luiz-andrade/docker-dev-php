0147e93f03bf286cb4b158521210d61c9a20ec24
Luiz <luizz_andrade@hotmail.com>
2021-11-09 02:32:03 -0400

    criado struturura inicial padrao mvc

---
+++ www/mvc/app/Controller/Pages/HomeController.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Controller\Pages;
+
+use \App\Utils\View;
+use \App\Model\Entity\Organization;
+
+class HomeController extends PageController
+{
+    /**
+     * Metodo responsavel por retornar o conteudo da home
+     * @return string
+     */
+    public static function getHome(){
+        //organizacao
+        $obOrganization = new Organization;
+
+        //view da home
+        $content = View::render('pages/home',[
+            'name' => $obOrganization->name,
+            'description' => $obOrganization->description,
+            'site' => $obOrganization->site
+        ]);
+    
+        return parent::getPage('PHP-MVC - Home', $content);
+    }
+    
+}
